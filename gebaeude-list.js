1c63abd25c852d0a96fcf77e8aa80c3f6a18ac16
Christopheruz <christopheruz@web.de>
2025-08-17 16:42:21 +0200

    Update gebaeude-list.js

--- gebaeude-list.js
+++ gebaeude-list.js
@@ -1,38 +1,38 @@
 window.GEBAEUDE_LIST = `
 1014 Parkhaus - Zahnklinik
 1280 Kinder- und Jugendpsychiatrie
-1300 Trafostation - Schmeddingstraߥ
+1300 Trafostation - Schmeddingstraße
 1930 Augenklinik
 1950 Betriebsarzt
 1970 Krankenpflegeschule-Modul
-1980 Geb䵤e Schmeddingstraߥ 60
-1990 Auߥnanlagen UKM Straߥn und Wege
+1980 Gebäude Schmeddingstraße 60
+1990 Außenanlagen UKM Straßen und Wege
 2100 Trafostation - Chirurgie
 2170 Chirurgische Klinik
 2171 Hubschrauberlandeplatz Chirurgie
 2180 Chirurgie-Technik
 2190 Haus Rosenbach
 2190 Gasflaschenlager - Haus Rosenbach
-2200 Geb䵤e Hüfferstraߥ 73-79
-2220 Geb䵤e Rishon-Le Zion-Ring 26
-2240 Geb䵤e Domagkstraߥ 11
-2250 Gastank - Domagkstraߥ 11
-2260 Gasflaschenlager - Domagkstraߥ 11
-2290 Geb䵤e Domagkstraߥ 19
+2200 Gebäude Hüfferstraße 73-79
+2220 Gebäude Rishon-Le Zion-Ring 26
+2240 Gebäude Domagkstraße 11
+2250 Gastank - Domagkstraße 11
+2260 Gasflaschenlager - Domagkstraße 11
+2290 Gebäude Domagkstraße 19
 2293 Klinische Virologie - Anmietung
-2300 G䲴nerei / ZTE
+2300 Gärtnerei / ZTE
 2310 Hals-, Nasen-, Ohren-Klinik - HG (HNO)
-2320 Studienhospital 
+2320 Studienhospital
 2330 Hals-, Nasen-, Ohren-Klinik - NG (HNO-NG)
 2351 Hautklinik
 2352 ZMBE
 2353 Hautklinik - Ostflügel
-2354 Fahrradst䮤er Haut-Ost
-2355 Fahrradst䮤er ZMBE
+2354 Fahrradständer Haut-Ost
+2355 Fahrradständer ZMBE
 2360 Tierstall - Hautklinik
-2370 G䲴nerei - Hautklinik
+2370 Gärtnerei - Hautklinik
 2380 ZTE (Neubau)
-2390 Geb䵤e Schmeddingstr.52
+2390 Gebäude Schmeddingstr.52
 2400 Klinikenkirche
 2420 ehem. Pfarramt
 2450 Trafostation - HVZ
@@ -40,73 +40,73 @@ window.GEBAEUDE_LIST = `
 2611 Hygiene
 2612 MTA-Schule / Med. Physik
 2613 MTR-Schule
-2614 Fahrradst䮤er
+2614 Fahrradständer
 2615 Massenspektrometer
 2620 Trafostation - Strahlenbiologie
-2621 Tumorbiologie 
-2622 Geb䵤e Robert-Koch-Straߥ 45
+2621 Tumorbiologie
+2622 Gebäude Robert-Koch-Straße 45
 2630 Gasflaschenlager - Inst. u. Schulen
-2630 Fahrradst䮤er - Inst. u. Schulen
+2630 Fahrradständer - Inst. u. Schulen
 2640 Gasflaschenlager - RKS 51
-2650 Geb䵤e Niels-Stensen-Straߥ 14
-2660 Geb䵤e Niels-Stensen-Straߥ 12
-2670 Geb䵤e Niels-Stensen-Straߥ 8
-2680 Geb䵤e Niels-Stensen-Straߥ 10
+2650 Gebäude Niels-Stensen-Straße 14
+2660 Gebäude Niels-Stensen-Straße 12
+2670 Gebäude Niels-Stensen-Straße 8
+2680 Gebäude Niels-Stensen-Straße 10
 2700 Kinderkrankenpflegeschule
-2710 Geb䵤e Robert-Koch-Straߥ 51
+2710 Gebäude Robert-Koch-Straße 51
 2730 Verwaltung des UKM ( VUK )
-2740 Fahrradst䮤er - Verwaltung
+2740 Fahrradständer - Verwaltung
 2750 MedForCe
-2760 Geb䵤e Domagkstraߥ 30
+2760 Gebäude Domagkstraße 30
 2770 Body & Brain Institut (BBIM)
-2790 Geb䵤e Domagkstr. 3 / Dekanat
+2790 Gebäude Domagkstr. 3 / Dekanat
 2800 Kinderpsychosomatik / -infektionsstation
 2810 Kinderdialysezentrum - Anmietung
-2821 Geb䵤e Domagkstraߥ 22
+2821 Gebäude Domagkstraße 22
 2822 Erlösmanagement
-2840 Geb䵤e Domagkstraߥ 20
+2840 Gebäude Domagkstraße 20
 2871 Erwachseneninfektionsstation
 2872 Forschungszentrum Med. Kliniken B und D
-2880 Trafostation und Garagen - Liegenschaften    
+2880 Trafostation und Garagen - Liegenschaften
 2900 Studienlabor
 2920 Servicezentrum
-2980 Ambulanz- und Forschungsmodul 
+2980 Ambulanz- und Forschungsmodul
 2990 Psychiatrie
 3080 Zentr. Tierexperimentelle Einrichtung ( ZTE )
 3090 Technikzentrale - Hautklinik
 3140 Medizin-Bibliothek Münster
-3250 Physiologie 
+3250 Physiologie
 3250 Gasflaschenlager - Physiologie
-3280 Geb䵤e Domagkstraߥ 14
+3280 Gebäude Domagkstraße 14
 3290 Krankenpflegeschule
 3300 PAN-Zentrum
-3340 Geb䵤e Von-Esmarch-Straߥ 62
-3350 Geb䵤e Von-Esmarch-Straߥ 62 - Pavillon
-3370 Fahrradst䮤er Med. Mikrobiologie
-3380 Med. Mikrobiologie - HG 
+3340 Gebäude Von-Esmarch-Straße 62
+3350 Gebäude Von-Esmarch-Straße 62 - Pavillon
+3370 Fahrradständer Med. Mikrobiologie
+3380 Med. Mikrobiologie - HG
 3390 Med. Mikrobiologie - NG
 3440 Humangenetik
-3450 Geb䵤e Pottkamp 17
+3450 Gebäude Pottkamp 17
 3480 Pathologie
 3500 Pharmakologie u. Toxikologie
 3510 Pharmakologie - Tierhaus
 3520 Kühlhaus - Umweltprobenbank
 3520 Gasflaschenlager-Kühlhaus-Umweltprobenbank
-3530 Parkhaus - Domagkstraߥ
-3540 Geb䵤e Waldeyerstraߥ 27
+3530 Parkhaus - Domagkstraße
+3540 Gebäude Waldeyerstraße 27
 3610 Physiologische Chemie
-3630 Geb䵤e Hittorfstraߥ 17
+3630 Gebäude Hittorfstraße 17
 3700 Sportmedizin
 3770 Zytostatika, ZLT
-3780 Fahrradst䮤er West - ZK
-3790 Fahrradst䮤er OST - ZK
+3780 Fahrradständer West - ZK
+3790 Fahrradständer OST - ZK
 3800 Zentralklinikum ( ZK )
 3800 IEZ
 3800 Trafostation ZK - Flachbereich Ost + West
 3801 Zentralklinikum (ZK) Modul Ost
 3810 Versorgungszentrum ( VZ )
 3820 Wertstoff- u.  Apothekenlager
-3830 Lehrgeb䵤e (LG)
+3830 Lehrgebäude (LG)
 3830 Trafostation - LG
 3840 Parkhaus - ZK
 3850 Pforte - Hauptzufahrt
@@ -114,43 +114,42 @@ window.GEBAEUDE_LIST = `
 3860 Trafostation - ZMK
 3870 Pforte - Hubschrauberlandeplatz
 3871 Hubschrauberlandeplatz VZ
-3910 Fahrradst䮤er - Parkpl䴺e - VZ
+3910 Fahrradständer - Parkplätze - VZ
 4210 Helen-Keller-Schule
-4220 Kindertagesst䴴e
-4240 Geb䵤e Schmeddingstraߥ 62 - 66
+4220 Kindertagesstätte
+4240 Gebäude Schmeddingstraße 62 - 66
 4560 Werkwohnungen
-7290 Auߥnanlagen UKM Grünfl䣨en
+7290 Außenanlagen UKM Grünflächen
 7400 Trafostation - Hautklinik
-7480 Geb䵤e Röntgenstraߥ 21
-7520 Med. Psychologie 
+7480 Gebäude Röntgenstraße 21
+7520 Med. Psychologie
 7530 Manuelle Medizin
 7540 Herz-MRT-Zentrum
 7560 Institut für Biomagnetismus und Biosignalanalyse
 7610 Knochenmarktransplantationszentrum (KMT)
-7630 Fahrradst䮤er KMT - Süd
+7630 Fahrradständer KMT - Süd
 7740 Center for Soft Nanoscience (SoN) - Anmietung
 7760 Nano-Bioanalytik-Zentrum (NBZ) - Anmietung
 7770 ICB - Anmietung
 7780 Technologiehof - Anmietung
-7960 Rechtsmedizin 
+7960 Rechtsmedizin
 7970 Rechtsmedizin - Leichenschauhaus
 8220 Feuerwache
 8230 UKM Akademie - Anmietung
-8250 Fahrradst䮤er Feuerwache
+8250 Fahrradständer Feuerwache
 8500 Tagesklinik KIJU Roxel - Anmietung
 8510 Depot Roxel - Anmietung
-8580 Tagungs- und G䳴ehaus (das Ox)
+8580 Tagungs- und Gästehaus (das Ox)
 8600 UKM-Praxis für Andrologie
-9000 Versorgungskan䬥 UKM
+9000 Versorgungskanäle UKM
 9801 Clemenshospital - Anmietung
 9802 St. Franziskus-Hospital - Anmietung
 9820 LBS - Anmietung
-9821 Geb䵤e Josef-Pieper-Straߥ 2 - Anmietung
-9824 Wilhelm-Schickard-Straߥ 2 - Anmietung
+9821 Gebäude Josef-Pieper-Straße 2 - Anmietung
+9824 Wilhelm-Schickard-Straße 2 - Anmietung
 9910 UKM Marienhospital Steinfurt GmbH -Borghorst-
 9911 Bauhaus MVZ GmbH Hauptstandort Steinfurt  
 9920 UKM Marienhospital Steinfurt GmbH -Emsdetten-
-9000 Versorgungskan䬥 UKM
-9805 Franziskus Carr饠Haus B - Anmietung
+9805 Franziskus Carrée Haus B - Anmietung
 `.trim().split(/\r?\n/);
 
